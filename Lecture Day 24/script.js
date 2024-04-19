28f804ef63255d9fdb60194c05c6b069534be32d
Muhammad Ahmad <162286232+AhmadGill7@users.noreply.github.com>
2024-04-20 00:32:54 +0500

    Lecture day 24 commit plus practice commit

---
+++ Lecture Day 24/script.js
@@ -0,0 +1,78 @@
+// let student = {
+//   neme : "ahmad",
+//   roll : 7007,
+//   institute : "Gamica",
+//   city : "Faisalabad",
+//   degree : "Full Stack Web dev"
+// }
+// console.log(student.degree)
+
+  
+  
+  
+  
+        //practice tasks below
+
+
+
+        
+// let threeTrues = [true, true, false,true, false, true, false];
+// let x = countTrue(threeTrues);
+// console.log(x);
+
+// function countTrue(arr) {
+//   a = 0;
+//   for (let i = 0; i < arr.length; i++) {
+  //     if (arr[i] == true) {
+    //       a++;
+    //     }
+    //   }
+    //   return a;
+    // }
+    
+
+
+    
+    // function nextEdge(side1, side2) {
+      // 	return (side1 + side2) -1;
+      // }
+
+      // Codewars Task
+// Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+// Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+// Example:
+
+// Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+// String.prototype.toJadenCase = function () {
+
+//   let sentence = this.split(" ");
+//   for (let i=0; i < sentence.length; i++) {
+//     sentence[i] = sentence[i][0].toUpperCase() + sentence[i].substr(1);
+// }
+
+//   return sentence.join(" ");
+
+// };
+// let quote = "lorem ipsum this that Flam flan and the sentence of the flan flan";
+// let x = quote.toJadenCase();
+// console.log(x);
+
+
+
+// Edabit task to check if there is 7 present in this array or not 
+
+// function sevenBoom(arr) {
+//   for (let num of arr) {
+//     if (String(num).includes("7")) {
+//       return "Boom!";
+//     }
+//   }
+//   return "There is no 7 in the array";
+// }
+// let arre = [125, 20, 30, 6, 80];
+// let x = sevenBoom(arre);
+// console.log(x);
