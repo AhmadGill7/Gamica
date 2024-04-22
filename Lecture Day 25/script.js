94f95513b7fdfc791ad2c4660692bd5a752327aa
Muhammad Ahmad <162286232+AhmadGill7@users.noreply.github.com>
2024-04-22 23:32:02 +0500

    Lecture Day 25 Day End Commit

---
+++ Lecture Day 25/script.js
@@ -0,0 +1,28 @@
+// let names = [2, true, "FSD", 50, false, {}, 400, []];
+
+// for (let i = 0; i < names.length; i++) {
+//   if (typeof names[i]!== 'number') {
+//     names.splice(i, 1);
+//     i--;
+//   }
+// }
+// console.log(names);
+
+// let names = [2,3,4, true, "Fsd", false, {}, 400, []];
+//   for (let i = 0; i < names.length; i++) {
+//     if (typeof names[i] === "number") {
+//       names.splice(i, 1);
+//     }
+//   }
+//   console.log(names);
+
+
+
+
+
+
+
+
+
+
+
